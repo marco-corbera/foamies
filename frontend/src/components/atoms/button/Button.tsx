35e7296a6eeee33aaa37002c10a37d166da42afb
Marco Corbera <marco.corbera1@gmail.com>
2024-11-21 13:30:05 -0500

    Add className prop to button

--- frontend/src/components/atoms/button/Button.tsx
+++ frontend/src/components/atoms/button/Button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = {
   onClick?: () => void;
   variant?: "primary" | "secondary";
   disabled?: boolean;
+  className?: string;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -12,6 +13,7 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   variant = "primary",
   disabled = false,
+  className = "",
 }) => {
   const baseClasses =
     "px-4 py-2 rounded-full text-white font-semibold transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2";
@@ -26,7 +28,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`${baseClasses} ${variantClasses} ${disabledClasses}`}
+      className={`${baseClasses} ${variantClasses} ${disabledClasses} ${className}`}
       disabled={disabled}
     >
       {children}
